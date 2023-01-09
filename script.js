45fd1b2515c9be74b647d7cbd6be3a2f941f5322
aguilax <yo1346>
2023-01-08 20:14:21 -0600

    clase03 terminada

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 const btn = document.querySelector("[data-form-btn]");
 
 const createTask = (evento) => {
+    // Evitar el comportamiento normal del evento, en este caso el click al elemento botón
     evento.preventDefault();
+    // Seleccionar el elemento input, donde se ingresa el texto de la tarea que se realizará
     const input = document.querySelector("[data-form-input]");
-    console.log(input.value);
+    // Obtener el texto que se ingreso en el input
+    const value = input.value;
+    // Establecer el valor del input en un string vacio
+    input.value = "";
+    // Seleccionar el elemento lista del html
+    const lista = document.querySelector("[data-lista]");
+    // Crear un nuevo li
+    const task = document.createElement("li");
+    // Añadir una clase al li creado arriba
+    task.classList.add("card");
+    // Al elemento lista agregar el li creado anteriormente
+    lista.appendChild(task);
+    // Establecer el contenido que se mostrará en el li
+    const contenido = `<div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${value}</span>
+  </div>
+  <i class="fas fa-trash-alt trashIcon icon"></i>`
+    // Agregar el contenido que debe llevar el li
+    task.innerHTML = contenido;
+
 }
 
 console.log(btn);
